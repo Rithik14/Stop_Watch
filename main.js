884d3018af5a0832005f4758369926c3ced045b3
Rithik k <95522883+Rithik14@users.noreply.github.com>
2023-03-17 13:33:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,78 @@
-const time_el = document.querySelector('.watch .time');
-const start_btn=document.getElementById('start');
-const stop_btn=document.getElementById("stop");
-const reset_btn=document.getElementById("reset");
-
-let seconds=0;
-let interval =null;
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
   
-// Event listeners
-start_btn.addEventListener('click',start);
-stop_btn.addEventListener('click',stop);
-reset_btn.addEventListener('click',reset);
-
-//Update the timer
-function timer(){
-    seconds++;
-
-    //format our time
-    let hrs  = Math.floor(seconds / 3600);
-    let mins = Math.floor((seconds-(hrs * 3600)) / 60);
-    let secs = seconds % 60;
-    
-    
-    if (secs < 10) secs ='0' + secs;
-    if (mins < 10) mins ="0" + mins;
-    if (hrs < 10) hrs ="0" + hrs;
-    
-    
-    time_el.innerText = `${hrs}:${mins}:${secs}`;
-}
-function start(){
-    if(interval){
-        return
+let hour = 00;
+let minute = 00;
+let second = 00;
+let count = 00;
+  
+startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+});
+  
+stopBtn.addEventListener('click', function () {
+    timer = false;
+});
+  
+resetBtn.addEventListener('click', function () {
+    timer = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = "00";
+    document.getElementById('min').innerHTML = "00";
+    document.getElementById('sec').innerHTML = "00";
+    document.getElementById('count').innerHTML = "00";
+});
+  
+function stopWatch() {
+    if (timer) {
+        count++;
+  
+        if (count == 100) {
+            second++;
+            count = 0;
+        }
+  
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+  
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+  
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        let countString = count;
+  
+        if (hour < 10) {
+            hrString = "0" + hrString;
+        }
+  
+        if (minute < 10) {
+            minString = "0" + minString;
+        }
+  
+        if (second < 10) {
+            secString = "0" + secString;
+        }
+  
+        if (count < 10) {
+            countString = "0" + countString;
+        }
+  
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+        document.getElementById('count').innerHTML = countString;
+        setTimeout(stopWatch, 10);
     }
-    interval = setInterval(timer, 1000);
-
-}
-function stop(){
-    clearInterval(interval);
-    interval=null;
-
-}
-function reset(){
-    stop();
-    seconds=0;
-    time_el.innerText='00:00:00';
 }
